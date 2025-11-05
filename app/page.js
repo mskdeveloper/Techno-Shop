0977c86239d9e026dd0c0a08ab31e4be7624a4a6
Mohammad Sadeq Kazemi <mskdev.ir@gmail.com>
2025-11-05 12:29:55 +0330

    eddit home page to resolve path

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 "use client";
 import React from "react";
 
-import Slider from "@/components/Home/slider";
-import Banners from "@/components/Home/Banners";
-import PopularProducts from "@/components/Home/PopularProducts";
+import Slider from "../components/Home/slider";
+import Banners from "../components/Home/Banners";
+import PopularProducts from "../components/Home/PopularProducts";
 
 const page = () => {
   return (
